5bb45172fc364cc5f87cf361396f325ff283d5f3
Felix-Puetz <FPuetz@gmx.net>
2021-06-30 21:14:36 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TheNavigation />
+  <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="px-4 py-6 sm:px-0">
+          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+        </div>
+        <!-- /End replace -->
+      </div>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheNavigation from './components/TheNavigation.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: { TheNavigation },
+});
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,124 @@
+<template>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center h-16">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="
+              inline-flex
+              items-center
+              justify-center
+              p-2
+              rounded-md
+              text-gray-400
+              hover:text-white hover:bg-gray-700
+              focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white
+            "
+          >
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="
+            flex-1 flex
+            items-center
+            justify-center
+            sm:items-stretch sm:justify-start
+            h-full
+            w-full
+          "
+        >
+          <div class="flex-shrink-0 flex items-center">
+            <img
+              class="block h-9 w-auto"
+              src="@/assets/logo-bw.svg"
+              alt="Felix Pütz"
+            />
+          </div>
+          <h1 class="flex items-center ml-5 text-white text-3xl tracking-wider">Felix Pütz</h1>
+          <div class="hidden sm:block ml-auto">
+            <div class="flex space-x-4 h-full items-center">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'px-3 py-2 rounded-md text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <a
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block px-3 py-2 rounded-md text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</a
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
+
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Skills", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+];
+
+export default defineComponent({
+  components: {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    BellIcon,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    const open = ref(false);
+
+    return {
+      navigation,
+      open,
+    };
+  },
+});
+</script>
