99b2eea07e0733c9b62a64b139e11b81e029f2b3
Felix-Puetz <FPuetz@gmx.net>
2021-06-30 21:45:29 +0200

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <TheNavigation />
   <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
-          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-        </div>
-        <!-- /End replace -->
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <!-- Replace with your content -->
+      <div class="px-4 py-6 sm:px-0">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
       </div>
-    </main>
+      <!-- /End replace -->
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
