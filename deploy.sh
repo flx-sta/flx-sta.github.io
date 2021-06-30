28f963b69f0cde339947c63ee0bc73c4963b8b6a
Felix-Puetz <FPuetz@gmx.net>
2021-06-30 21:20:56 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:Felix-Puetz/Felix-Puetz.github.io.git main
 
 cd -
