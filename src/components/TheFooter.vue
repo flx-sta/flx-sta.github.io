1f12490efda9b74027ffbd18be98914037173b5a
Felix-Puetz <FPuetz@gmx.net>
2021-08-20 12:47:46 +0200

    adjust contact mail

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -22,12 +22,7 @@
         icon="envelope"
         class="mr-1 opacity-0 group-hover:opacity-100 transition"
       ></fa-icon>
-      <a
-        :href="`mailto:fpuetz@gmx.net?subject=${t(
-          'contact',
-        )} via felix-puetz.com`"
-        target="_blank"
-      >
+      <a :href="`mailto:${mail.to}?subject=${mail.subject}`" target="_blank">
         {{ t('contact') }}
       </a>
     </div>
@@ -41,10 +36,15 @@ import { useI18n } from 'vue-i18n';
 export default defineComponent({
   setup() {
     const { t } = useI18n({ inheritLocale: true });
+    const mail = {
+      to: 'contact@felix-puetz.com',
+      subject: `${t('contact')} via felix-puetz.com`,
+    };
 
     return {
       t,
       fullYear: new Date().getFullYear(),
+      mail,
     };
   },
 });
